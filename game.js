3357dd8c1009e861cc5c117d39e30ebe17db0ad3
zakaria-altammo <zakaria.altammo@gmail.com>
2021-05-02 13:04:35 +0200

    finish the script

--- game.js
+++ game.js
@@ -16,3 +16,63 @@ var gameboard = document.getElementById("gameboard");
 var nav = document.getElementById("nav");
 
 gameStart();
+//Create images tags inside divs using for loop.
+function gameStart() {
+    shuffleArray(imgDuble);
+    gameboard.innerHTML = "";
+    for (var i = 0; i <= (imgDuble.length) - 1; i++) {
+        gameboard.innerHTML += '<div><img id="img ' + i + '" src="images/bg.png" onclick="compare(\'' + imgDuble[i] + '\',\'' + i + '\',this);" class="imgStyle"></div>';
+    }
+}
+
+//Compared images if there is a match keep the images flipped.
+function compare(imgSrc, num, imgTag) {
+    if (cardsFlippedOver < 2 && lastCardPicked != num) {
+        flipArray[cardsFlippedOver] = imgDuble[num];
+        flipArray[(cardsFlippedOver + 2)] = imgTag.id;
+        cardsFlippedOver++;
+        imgTag.src = imgDuble[num];
+        console.log(imgSrc);
+        if (cardsFlippedOver == 2) {
+            if (flipArray[0] == flipArray[1]) {
+                console.log('Match Found');
+                nav.innerHTML = 'Score : ' + Score++;
+                choseAgain();
+            } else {
+                timer = setInterval(hideImg, 2000);
+                console.log('Match Not Found');
+            }
+        }
+        lastCardPicked = num;
+    }
+}
+
+//Clear game action.
+function choseAgain() {
+    cardsFlippedOver = 0;
+    flipArray = [];
+    clearInterval(timer);
+}
+
+
+//After comparing the two images there is a difference return image flip over and chose again.
+function hideImg() {
+    if (flipArray[2]) {
+        document.getElementById(flipArray[2]).src = "images/bg.png";
+    }
+    if (flipArray[3]) {
+        document.getElementById(flipArray[3]).src = "images/bg.png";
+    }
+    choseAgain();
+}
+
+//Shuffling the images each time loading.
+function shuffleArray(d) {
+    for (var c = d.length - 1; c > 0; c--) {
+        var b = Math.floor(Math.random() * (c + 1));
+        var a = d[c];
+        d[c] = d[b];
+        d[b] = a;
+    }
+    return d;
+}
